ec131bff9bdee8f2b594a47aab5364685c3a58a6
María López <37983313+oppahero@users.noreply.github.com>
2023-11-13 13:36:21 -0400

    Page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,98 @@
-'use client'
 
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.main} role='main'>
+
+      <div className={styles.containerImages}>
+
+        <div className={styles.backgroundImg}>
+
+          <div className={styles.containerPadding}>
+
+            <div className={styles.containerCards}>
+
+              <Image
+                className={styles.containerCards__cardBack}
+                src="/images/bg-card-back.png"
+                alt="Back of the card"
+                width={286}
+                height={158}
+                priority
+              />
+
+              <div className={styles.containerCards__cardFront}>
+                {/* <div className={styles.containerCards__cardFront__content}> */}
+                  <div className={styles.containerCards__cardFront__circles}>
+                    <Image
+                        src="/images/card-logo.svg"
+                        alt="Card logo"
+                        width={58}
+                        height={32}
+                        priority
+                    />
+                  </div>
+                  <p className={styles.containerCards__cardFront__numbers}>
+                    9591648963891011
+                  </p>
+                  <div className={styles.containerCards__cardFront__data}>
+                    <p id='name'>FELICIA LEIRE</p>
+                    <p id='date'>09/26</p>
+                  </div>
+                {/* </div> */}
+              </div>
+              
+            </div>
+           
+          </div>
+
         </div>
+        
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.containerForm}>
 
+        <form className={styles.form}>
+
+          <div className={styles.formInput}>
+            <label htmlFor="name">Cardholder name</label>
+            <input type="text" name="name" id="name" placeholder='e.g. Jane Appleseed' />
+            {/* <span className='errorMessage'>Can´t be blank</span> */}
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="card-number">Card number</label>
+            <input type="text" name="card-number" id="card-number" placeholder='e.g. 1234 5678 9123 0000' />
+            {/* <span className='errorMessage'>Can´t be blank</span>
+            <span className='errorMessage'>Wront format, numbers only</span> */}
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="card-number">Exp. Date (MM/YY)  CVC</label>
+            <div className={`${styles.inputGroup}  ${styles.inputGroup__gap12}`}>
+              <div className={`row50 ${styles.inputGroup}  ${styles.inputGroup__gap9}`}>
+                <div className={styles.formInput}>
+                  <input type="text" name="month" id="month" placeholder='MM' />
+                  {/* <span className='errorMessage'>Can´t be blank</span> */}
+                </div>
+                <div className={styles.formInput}>
+                  <input type="text" name="year" id="year" placeholder='YY' />
+                  {/* <span className='errorMessage'>Can´t be blank</span> */}
+                </div>
+              </div>
+              <div className="row50">
+                <div className={styles.formInput}>
+                  <input type="text" name="cvc" id="cvc" placeholder='e.g. 123' />
+                  {/* <span className='errorMessage'>Can´t be blank</span> */}
+                </div>
+              </div>
+            </div>
+          </div>
+          <button>Confirm</button>
+        </form>
+      </div>
     </main>
   )
 }
