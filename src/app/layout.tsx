20bf27a832bdebdfba46483da0f48f4e7a624ad8
María López <37983313+oppahero@users.noreply.github.com>
2023-11-13 13:36:45 -0400

    Google Font Spcae Grotesk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next'
 import { Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
 import './globals.css'
 
+
 const spaceGrotesk = Space_Grotesk({
-  weight: '500',
+  weight: '400',
   subsets: ['latin'],
 })
-
 
 export const metadata: Metadata = {
   title: 'Interactive card details form by María López',
   description: 'FrontendMentor challenge',
 }
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={spaceGrotesk.className}>{children}</body>
