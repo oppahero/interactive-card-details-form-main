b599091ab1b7f9292bdb14897d7e4dd14fc8529c
María López <37983313+oppahero@users.noreply.github.com>
2023-11-17 13:49:01 -0400

    Submit state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
       </div>
 
       <div className={styles.containerForm}>
-        {!submit
+        {submit
           ? <div className={styles.containerMessage}>
             <Image
               className={styles.iconComplete}
