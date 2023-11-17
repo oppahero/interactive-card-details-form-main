03c413d7f826a407e5f45327b49a5b31cc0d763a
María López <37983313+oppahero@users.noreply.github.com>
2023-11-17 11:56:23 -0400

    Success message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 import styles from "./page.module.css";
 import { useFormik } from "formik";
 import { useState } from "react";
+import Image from 'next/image'
 import * as Yup from "yup";
-
 
 const FormSchema = Yup.object({
   cardName: Yup.string()
@@ -29,6 +29,16 @@ const FormSchema = Yup.object({
 
 export default function Home() {
 
+  const [cardValues, setCardValues] = useState({
+    cardName: "ane Appleseed",
+    cardNumber: "0000 0000 0000 0000",
+    month: "00",
+    year: "00",
+    cvc: "000",
+  });
+
+  const [submit, setSubmit] = useState(false)
+
   const { handleSubmit, errors, touched, getFieldProps } = useFormik({
     initialValues: {
       cardName: "",
@@ -40,15 +50,8 @@ export default function Home() {
     validationSchema: FormSchema,
     onSubmit: (values) => {
       console.log(values);
+      setSubmit(true)
     },
-  });
-
-  const [cardValues, setCardValues] = useState({
-    cardName: "ane Appleseed",
-    cardNumber: "0000 0000 0000 0000",
-    month: "00",
-    year: "00",
-    cvc: "000",
   });
 
   const formatCardNumber = (cardNumber: string) => {
@@ -103,93 +106,108 @@ export default function Home() {
       </div>
 
       <div className={styles.containerForm}>
-        <form className={styles.form} noValidate onSubmit={handleSubmit}>
-          <div className={styles.formInput}>
-            <label htmlFor="cardName">Cardholder name</label>
-            <input
-              type="text"
-              id="cardName"
-              placeholder="e.g. Jane Appleseed"
-              autoComplete="off"
-              onKeyUp={handleChange}
-              {...getFieldProps("cardName")}
-              className={`${touched.cardName && errors.cardName}`}
+        {!submit
+          ? <div className={styles.containerMessage}>
+            <Image
+              className={styles.iconComplete}
+              src="/images/icon-complete.svg"
+              alt="Icon complete"
+              width={82}
+              height={82}
             />
-            {touched.cardName && errors.cardName && (
-              <span className="errorMessage">{errors.cardName}</span>
-            )}
+            <h2>THANK YOU!</h2>
+            <p>We&apos;ve added your card details!</p>
+            <button>Continue</button>
           </div>
-          <div className={styles.formInput}>
-            <label htmlFor="cardNumber">Card number</label>
-            <input
-              type="text"
-              id="cardNumber"
-              inputMode="numeric"
-              maxLength={16}
-              placeholder="e.g. 1234 5678 9123 0000"
-              onKeyUp={handleChange}
-              {...getFieldProps("cardNumber")}
-              className={`${touched.cardNumber && errors.cardNumber}`}
-            />
-            {touched.cardNumber && errors.cardNumber && (
-              <span className="errorMessage">{errors.cardNumber}</span>
-            )}
-          </div>
-          <div className={styles.formInput}>
-            <label htmlFor="month">Exp. Date (MM/YY) CVC</label>
-            <div
-              className={`${styles.inputGroup}  ${styles.inputGroup__gap12}`}
-            >
-              <div className={`row50 ${styles.inputGroup}  ${styles.inputGroup__gap9}`} >
-                <div className={styles.formInput}>
-                  <input
-                    type="text"
-                    maxLength={2}
-                    id="month"
-                    placeholder="MM"
-                    onKeyUp={handleChange}
-                    {...getFieldProps("month")}
-                    className={`${touched.month && errors.month}`}
-                  />
-                  {touched.month && errors.month && (
-                    <span className="errorMessage">{errors.month}</span>
-                  )}
+          : <form className={styles.form} noValidate onSubmit={handleSubmit}>
+            <div className={styles.formInput}>
+              <label htmlFor="cardName">Cardholder name</label>
+              <input
+                type="text"
+                id="cardName"
+                placeholder="e.g. Jane Appleseed"
+                autoComplete="off"
+                onKeyUp={handleChange}
+                {...getFieldProps("cardName")}
+                className={`${touched.cardName && errors.cardName}`}
+              />
+              {touched.cardName && errors.cardName && (
+                <span className="errorMessage">{errors.cardName}</span>
+              )}
+            </div>
+            <div className={styles.formInput}>
+              <label htmlFor="cardNumber">Card number</label>
+              <input
+                type="text"
+                id="cardNumber"
+                inputMode="numeric"
+                maxLength={16}
+                placeholder="e.g. 1234 5678 9123 0000"
+                onKeyUp={handleChange}
+                {...getFieldProps("cardNumber")}
+                className={`${touched.cardNumber && errors.cardNumber}`}
+              />
+              {touched.cardNumber && errors.cardNumber && (
+                <span className="errorMessage">{errors.cardNumber}</span>
+              )}
+            </div>
+            <div className={styles.formInput}>
+              <label htmlFor="month">Exp. Date (MM/YY) CVC</label>
+              <div
+                className={`${styles.inputGroup}  ${styles.inputGroup__gap12}`}
+              >
+                <div className={`row50 ${styles.inputGroup}  ${styles.inputGroup__gap9}`} >
+                  <div className={styles.formInput}>
+                    <input
+                      type="text"
+                      maxLength={2}
+                      id="month"
+                      placeholder="MM"
+                      onKeyUp={handleChange}
+                      {...getFieldProps("month")}
+                      className={`${touched.month && errors.month}`}
+                    />
+                    {touched.month && errors.month && (
+                      <span className="errorMessage">{errors.month}</span>
+                    )}
+                  </div>
+                  <div className={styles.formInput}>
+                    <input
+                      type="text"
+                      maxLength={2}
+                      id="year"
+                      placeholder="YY"
+                      onKeyUp={handleChange}
+                      {...getFieldProps("year")}
+                      className={`${touched.year && errors.year}`}
+                    />
+                    {touched.year && errors.year && (
+                      <span className="errorMessage">{errors.year}</span>
+                    )}
+                  </div>
                 </div>
-                <div className={styles.formInput}>
-                  <input
-                    type="text"
-                    maxLength={2}
-                    id="year"
-                    placeholder="YY"
-                    onKeyUp={handleChange}
-                    {...getFieldProps("year")}
-                    className={`${touched.year && errors.year}`}
-                  />
-                  {touched.year && errors.year && (
-                    <span className="errorMessage">{errors.year}</span>
-                  )}
-                </div>
-              </div>
-              <div className="row50">
-                <div className={styles.formInput}>
-                  <input
-                    type="text"
-                    maxLength={3}
-                    id="cvc"
-                    placeholder="e.g. 123"
-                    onKeyUp={handleChange}
-                    {...getFieldProps("cvc")}
-                    className={`${touched.cvc && errors.cvc}`}
-                  />
-                  {touched.cvc && errors.cvc && (
-                    <span className="errorMessage">{errors.cvc}</span>
-                  )}
+                <div className="row50">
+                  <div className={styles.formInput}>
+                    <input
+                      type="text"
+                      maxLength={3}
+                      id="cvc"
+                      placeholder="e.g. 123"
+                      onKeyUp={handleChange}
+                      {...getFieldProps("cvc")}
+                      className={`${touched.cvc && errors.cvc}`}
+                    />
+                    {touched.cvc && errors.cvc && (
+                      <span className="errorMessage">{errors.cvc}</span>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <button type="submit">Confirm</button>
-        </form>
+            <button type="submit">Confirm</button>
+          </form>
+        }
+
       </div>
     </main>
   );
